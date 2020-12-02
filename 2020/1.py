ee70e1c245c92322c42f336d63eec58cca8726fe
jholty66 <burholtcharlie@gmail.com>
2020-12-02 09:37:56 +0000

    Day 1 complete.

---
+++ 2020/1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Part 1.
+I=open("1.in").read().split("\n")
+I=[int(x) for x in I]
+
+for i in range(len(I)):
+    for j in range(i,len(I)):
+        if I[i]+I[j] == 2020:
+            print(I[i]*I[j])
+
+# Part 2.
+for i in range(len(I)):
+    for j in range(i,len(I)):
+        for k in range(j,len(I)):
+            if I[i]+I[j]+I[k] == 2020:
+                print(I[i]*I[j]*I[k])
