afa540defc14240257adb8ccb6d9294b2797a3ca
jholty66 <burholtcharliegmail.com>
2020-12-02 22:12:18 +0000

    Made some progress.

--- 2020/2.py
+++ 2020/2.py
@@ -3,13 +3,12 @@
 I=open("2.in").read().split("\n")
 I=[l.split(" ") for l in I]
 
-# Part 1.
-ans = 0
 for password in I:
-    bounds = [int(i) for i in password[0].split('-')]
-    if password[2].count(password[1][0]) in range(bounds[0],bounds[-1]+1):
-        ans += 1
+    password[0] = [int(i) for i in password[0].split('-')]
+    password[1] = password[1][:-1]
 
-print(ans)
+# Part 1.
+print(sum([1 for password in I if password[2].count(password[1][0]) in range(password[0][0],password[0][1]+1)]))
 
 # Part 2.
+print(sum([(password[2][password[0][0]-1] == password[1]) != (password[2][password[0][1]-1] == password[1]) for password in I]))
