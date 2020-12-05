b63f2becd19998e8f58610ae391c181971f9b058
jholty66 <burholtcharliegmail.com>
2020-12-05 13:25:02 +0000

    Day 5.

---
+++ 2020/5.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+input=open('5.in').read().split("\n")
+
+def part(lst,dir):
+    if dir in ('F','L'):
+        return lst[:int(len(lst)/2)]
+    elif dir in ('B','R'):
+        return lst[int(len(lst)/2):]
+
+# Part 1.
+def findSeat(seat):
+    y = list(range(128))
+    x = list(range(8))
+    for char in seat[:7]:
+        y = part(y,char)
+    for char in seat[-3:]:
+        x = part(x,char)
+    return y.pop(),x.pop()
+
+ids = []
+for seat in input:
+    row, column = findSeat(seat)
+    ids.append(row * 8 + column)
+
+# 983 is too high.
+print(max(ids))
+
+# Part 2.
+ids = sorted(ids)
+for i in range(1,len(ids)):
+    if abs(ids[i-1] - ids[i]) != 1:
+        print(ids[i] - 1)
+        break
