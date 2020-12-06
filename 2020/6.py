ad500787904fb1a3a77a8f84f08bb140b7fd0684
jholty66 <burholtcharliegmail.com>
2020-12-06 17:57:49 +0000

    Day 6.

--- 2020/6.py
+++ 2020/6.py
@@ -6,8 +6,3 @@ print(sum(map(lambda g: len(g), [set(''.join(group)) for group in input])))
 
 # Part 2.
 print(sum(map(lambda g: len(set(g[0]).intersection(*g)), input)))
-
-# Golfed
-input=list(map(lambda group: group.split('\n'),open('6.in').read().split('\n\n')))
-print(sum(map(lambda g: len(g),[set(''.join(group)) for group in input]))) # Part 1.
-print(sum(map(lambda g: len(set(g[0]).intersection(*g)),input))) # Part 2.
