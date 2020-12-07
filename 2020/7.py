e81027987851a45e587a97d3558abeaac1d6c29f
jholty66 <burholtcharliegmail.com>
2020-12-07 23:24:31 +0000

    Day 7, Part 1.

---
+++ 2020/7.py
@@ -0,0 +1,28 @@
+from typing import List
+
+import re
+
+input = open('7.in').read().split('\n')
+
+# Part 1.
+def formatRule(rule: str) -> list[str, list[str]]:
+    rule = re.sub('bags?\s?', '', rule)[:-2].split(' contain ')
+    rule[1] = [re.findall('[a-z]+\s[a-z]+$', bag)[0] for bag in rule[1].split(' , ')]
+    return rule
+
+rules = list(map(lambda rule: formatRule(rule), input))
+
+def findBags(rules: list[str], bags: list[str]) -> int:
+    matches = 0
+    newbags = []
+    for rule in rules:
+        if any(bag in rule[1] for bag in bags) and not any(rule[0] == bag for bag in bags):
+            matches += 1
+            newbags.append(rule[0])
+    bags += newbags
+    if matches == 0:
+        return len(bags) - 1
+    else:
+        return findBags(rules, bags)
+
+print(findBags(rules, ['shiny gold']))
