7f7407bd63c988549bc50cded321d01a6cd92b02
jholty66 <burholtcharlie@gmail.com>
2020-12-08 23:22:19 +0000

    Attempt at day 7 and 8.

--- 2020/7.py
+++ 2020/7.py
@@ -1,14 +1,14 @@
 import re
 
-input = open('7.in').read().split('\n')
+input = open('7.txt').read().split('\n')
 
 # Part 1.
-def formatRule(rule: str) -> list[str, list[str]]:
+def lr(rule: str) -> list[str, list[str]]:
     rule = re.sub('bags?\s?', '', rule)[:-2].split(' contain ')
     rule[1] = [re.findall('[a-z]+\s[a-z]+$', bag)[0] for bag in rule[1].split(' , ')]
     return rule
 
-rules = list(map(lambda rule: formatRule(rule), input))
+rules = list(map(lambda rule: lr(rule), input))
 
 def findBags(rules: list[str], bags: list[str]) -> int:
     matches = 0
@@ -24,3 +24,27 @@ def findBags(rules: list[str], bags: list[str]) -> int:
         return findBags(rules, bags)
 
 print(findBags(rules, ['shiny gold']))
+
+# Part 2.
+def fr(rule: str) -> tuple[str, list[list[int, str]]]:
+    rule = re.sub('bags?\s?', '', rule)[:-2].split(' contain ')
+    x = [c.split(' ', 1) for c in rule[1].split(' , ')]
+    for y in range(len(x)):
+        if x[y][0] == 'no':
+            x[y][0] = 0
+        else:
+            x[y][0] = int(x[y][0])
+    rule[1] = x
+    return tuple(rule)
+
+rules = dict(map(lambda rule: fr(rule), input))
+
+def countBags(rules: dict[str, dict[int, str]], bag: str) -> int:
+    bags = [b for b in rules[bag] if b[1] != 'other']
+    if len(bags) == 0:
+        return 1
+    else:
+        return sum(bag[0] * countBags(rules, bag[1]) for bag in bags)
+
+# lb 16614
+print(countBags(rules, 'shiny gold'))
---
+++ 2020/8.py
@@ -0,0 +1,60 @@
+pi = open('8.in').read().split('\n')
+
+instructions = []
+for i in range(len(pi)):
+    instructions.append(pi[i].split(' '))
+
+# First instruction starts at 0.
+# Part 1.
+counter = acc = 0
+ei = [0]
+
+while True:
+    if instructions[counter][0] == 'acc':
+        acc += eval(instructions[counter][1])
+        counter += 1
+    elif instructions[counter][0] == 'jmp':
+        counter += eval(instructions[counter][1])
+    elif instructions[counter][0] == 'nop':
+        counter += 1
+    else:
+        print('Unknown instruction.')
+    if counter in ei:
+        break
+    else:
+        ei.append(counter)
+print(acc)
+
+# Part 2.
+def testInstruction(instructions: list[list[str, str]]) -> int:
+    instructions = str(instructions)
+    for i in range(len(instructions)):
+        ni = eval(instructions)
+        print(ni[i][0])
+        if ni[i][0] == 'jmp':
+            if eval(ni[i][1]) != 0:
+                ni[i][0] = 'nop'
+        elif ni[i][0] == 'nop':
+            ni[i][0] = 'jmp'
+
+        acc = 0
+        counter = 0
+        ei [0]
+        while True:
+            if ni[counter][0] == 'acc':
+                acc += eval(ni[counter][1])
+                counter += 1
+            elif ni[counter][0] == 'jmp':
+                counter += eval(ni[counter][1])
+            elif ni[counter][0] == 'nop':
+                counter += 1
+            else:
+                print('Unknown instruction.')
+            if counter in ei:
+                break
+            elif counter == 624:
+                return acc
+            else:
+                ei.append(counter)
+
+print(testInstruction(instructions))
